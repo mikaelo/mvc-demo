c79f944f5127cfa26e9e5166f191673c109f2ea9
Mikhail Ushanov <mikhail.ushanov@gmail.com>
2013-03-05 22:40:32 +0400

    AcionName attribute, @Html.HttpMethodOverride()
    AcionName - attribute to override action name, we can make same action
    name for different httpverbs
    @Html.HttpMethodOverride - can override Html.BeginForm post method -> to
    Put for example

---
+++ MvcDemo/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult MakeRequest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("MakeRequest")]
+        public ActionResult MakeRequest_Post(string request)
+        {
+            return Content("Post request " + request);
+        }
+
+        [HttpPut]
+        [ActionName("MakeRequest")]
+
+        public ActionResult MakeRequest_Put(string request)
+        {
+            return Content("Put request " + request);
+        }
+
+
+    }
+}
